33f2d6ee9b37796e7ec68ccadfa3f2f30898c813
Max Meyer <max.meyer@flane.de>
2026-08-21 13:40:27 +0200

    Implement Serilog logging configuration and update appsettings

--- src/Server/Server.API/Program.cs
+++ src/Server/Server.API/Program.cs
@@ -1,10 +1,18 @@
+using Serilog;
+
 var builder = WebApplication.CreateSlimBuilder(args);
+
+// Configure Serilog
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration.ReadFrom.Configuration(context.Configuration);
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    
+    app.UseDeveloperExceptionPage();
 }
 
 app.Run();
