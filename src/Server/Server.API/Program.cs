835bfe38b774ce8d22172b040efdc5161160cf9b
Max Meyer <max.meyer@flane.de>
2026-08-21 14:03:27 +0200

    add health checks and Entity Framework Core

--- src/Server/Server.API/Program.cs
+++ src/Server/Server.API/Program.cs
@@ -1,6 +1,9 @@
 using Serilog;
 
+// Add services to the container.
 var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.Services.AddHealthChecks();
 
 // Configure Serilog
 builder.Host.UseSerilog((context, configuration) =>
@@ -8,6 +11,7 @@ builder.Host.UseSerilog((context, configuration) =>
     configuration.ReadFrom.Configuration(context.Configuration);
 });
 
+// Build the application
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -15,4 +19,5 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
+app.MapHealthChecks("/health");
 app.Run();
