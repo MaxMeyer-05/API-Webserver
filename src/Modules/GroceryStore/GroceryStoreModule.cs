e0da706ba3780d34f414e613e3c85617ac57e0fd
MaxMeyer-05 <maxmeyer2411@gmail.com>
2026-08-23 10:05:20 +0200

    feat(grocery-store): Implement Grocery Store module with service registration and mappings

--- src/Modules/GroceryStore/GroceryStoreModule.cs
+++ src/Modules/GroceryStore/GroceryStoreModule.cs
@@ -1,5 +1,22 @@
 using SharedKernel.Modules;
 
+using GroceryStore.Features.Allergens;
+using GroceryStore.Features.Allergens.Interfaces;
+using GroceryStore.Features.Categories;
+using GroceryStore.Features.Categories.Interfaces;
+using GroceryStore.Features.Ingredients;
+using GroceryStore.Features.Ingredients.Interfaces;
+using GroceryStore.Features.Locations;
+using GroceryStore.Features.Locations.Interfaces;
+using GroceryStore.Features.Orders;
+using GroceryStore.Features.Orders.Interfaces;
+using GroceryStore.Features.Recipes;
+using GroceryStore.Features.Recipes.Interfaces;
+using GroceryStore.Features.Suppliers;
+using GroceryStore.Features.Suppliers.Interfaces;
+using GroceryStore.Features.Users;
+using GroceryStore.Features.Users.Interfaces;
+
 namespace GroceryStore;
 
 /// <summary>
@@ -17,5 +34,41 @@ public sealed class GroceryStoreModule : IModule
     /// <inheritdoc />
     public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
     {
+        services.AddControllers()
+                .AddApplicationPart(typeof(AllergenController).Assembly)
+                .AddApplicationPart(typeof(CategoryController).Assembly)
+                .AddApplicationPart(typeof(IngredientController).Assembly)
+                .AddApplicationPart(typeof(LocationController).Assembly)
+                .AddApplicationPart(typeof(OrderController).Assembly)
+                .AddApplicationPart(typeof(RecipeController).Assembly)
+                .AddApplicationPart(typeof(SupplierController).Assembly)
+                .AddApplicationPart(typeof(UserController).Assembly);
+
+            services.AddSingleton<IAllergenMapper, AllergenMapper>();
+            services.AddSingleton<ICategoryMapper, CategoryMapper>();
+            services.AddSingleton<IIngredientMapper, IngredientMapper>();
+            services.AddSingleton<ILocationMapper, LocationMapper>();
+            services.AddSingleton<IOrderMapper, OrderMapper>();
+            services.AddSingleton<IRecipeMapper, RecipeMapper>();
+            services.AddSingleton<ISupplierMapper, SupplierMapper>();
+            services.AddSingleton<IUserMapper, UserMapper>();
+
+            services.AddScoped<IAllergenRepository, AllergenRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IIngredientRepository, IngredientRepository>();
+            services.AddScoped<ILocationRepository, LocationRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IRecipeRepository, RecipeRepository>();
+            services.AddScoped<ISupplierRepository, SupplierRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            services.AddScoped<AllergenService>();
+            services.AddScoped<CategoryService>();
+            services.AddScoped<IngredientService>();
+            services.AddScoped<LocationService>();
+            services.AddScoped<OrderService>();
+            services.AddScoped<RecipeService>();
+            services.AddScoped<SupplierService>();
+            services.AddScoped<UserService>();
     }
 }
--- tests/Modules/GroceryStore.Tests/GroceryStoreModuleTest.cs
+++ tests/Modules/GroceryStore.Tests/GroceryStoreModuleTest.cs
@@ -1,6 +1,30 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+using GroceryStore.Features.Allergens;
+using GroceryStore.Features.Allergens.Interfaces;
+
+using GroceryStore.Features.Categories;
+using GroceryStore.Features.Categories.Interfaces;
+
+using GroceryStore.Features.Ingredients;
+using GroceryStore.Features.Ingredients.Interfaces;
+
+using GroceryStore.Features.Locations;
+using GroceryStore.Features.Locations.Interfaces;
+
+using GroceryStore.Features.Orders;
+using GroceryStore.Features.Orders.Interfaces;
+
+using GroceryStore.Features.Recipes;
+using GroceryStore.Features.Recipes.Interfaces;
+
+using GroceryStore.Features.Suppliers;
+using GroceryStore.Features.Suppliers.Interfaces;
+
+using GroceryStore.Features.Users;
+using GroceryStore.Features.Users.Interfaces;
+
 using SharedKernel.Modules;
 
 namespace GroceryStore.Tests;
@@ -44,6 +68,56 @@ public class GroceryStoreModuleTest
         var exception = Record.Exception(() => module.ConfigureServices(services, configuration));
         Assert.Null(exception);
     }
+
+    [Theory]
+    [Trait("Feature", "ServiceRegistration")]
+    [MemberData(nameof(ServiceRegistrations))]
+    public void ConfigureServices_ShouldRegisterService(
+        Type serviceType,
+        Type implementationType,
+        ServiceLifetime lifetime)
+    {
+        // Arrange
+        var module = new GroceryStoreModule();
+        var services = new ServiceCollection();
+        var configuration = new ConfigurationBuilder().Build();
+
+        // Act
+        module.ConfigureServices(services, configuration);
+
+        // Assert
+        var descriptor = Assert.Single(services, service => service.ServiceType == serviceType);
+        Assert.Equal(implementationType, descriptor.ImplementationType);
+        Assert.Equal(lifetime, descriptor.Lifetime);
+    }
+
+    public static TheoryData<Type, Type, ServiceLifetime> ServiceRegistrations => new()
+    {
+        { typeof(IAllergenMapper), typeof(AllergenMapper), ServiceLifetime.Singleton },
+        { typeof(ICategoryMapper), typeof(CategoryMapper), ServiceLifetime.Singleton },
+        { typeof(IIngredientMapper), typeof(IngredientMapper), ServiceLifetime.Singleton },
+        { typeof(ILocationMapper), typeof(LocationMapper), ServiceLifetime.Singleton },
+        { typeof(IOrderMapper), typeof(OrderMapper), ServiceLifetime.Singleton },
+        { typeof(IRecipeMapper), typeof(RecipeMapper), ServiceLifetime.Singleton },
+        { typeof(ISupplierMapper), typeof(SupplierMapper), ServiceLifetime.Singleton },
+        { typeof(IUserMapper), typeof(UserMapper), ServiceLifetime.Singleton },
+        { typeof(IAllergenRepository), typeof(AllergenRepository), ServiceLifetime.Scoped },
+        { typeof(ICategoryRepository), typeof(CategoryRepository), ServiceLifetime.Scoped },
+        { typeof(IIngredientRepository), typeof(IngredientRepository), ServiceLifetime.Scoped },
+        { typeof(ILocationRepository), typeof(LocationRepository), ServiceLifetime.Scoped },
+        { typeof(IOrderRepository), typeof(OrderRepository), ServiceLifetime.Scoped },
+        { typeof(IRecipeRepository), typeof(RecipeRepository), ServiceLifetime.Scoped },
+        { typeof(ISupplierRepository), typeof(SupplierRepository), ServiceLifetime.Scoped },
+        { typeof(IUserRepository), typeof(UserRepository), ServiceLifetime.Scoped },
+        { typeof(AllergenService), typeof(AllergenService), ServiceLifetime.Scoped },
+        { typeof(CategoryService), typeof(CategoryService), ServiceLifetime.Scoped },
+        { typeof(IngredientService), typeof(IngredientService), ServiceLifetime.Scoped },
+        { typeof(LocationService), typeof(LocationService), ServiceLifetime.Scoped },
+        { typeof(OrderService), typeof(OrderService), ServiceLifetime.Scoped },
+        { typeof(RecipeService), typeof(RecipeService), ServiceLifetime.Scoped },
+        { typeof(SupplierService), typeof(SupplierService), ServiceLifetime.Scoped },
+        { typeof(UserService), typeof(UserService), ServiceLifetime.Scoped }
+    };
 
     #endregion
 }
